63fd6addc305ba46fc6ccdf9fc4b96360fb436b7
Olof Astrand <olof.astrand@gmail.com>
2020-04-30 20:15:44 +0200

    Updated cmake file for libsigrok

--- main/app_main.c
+++ main/app_main.c
@@ -27,7 +27,7 @@ SOFTWARE.
 #include "esp_wifi.h"
 #include "esp_system.h"
 #include "esp_event.h"
-#include "esp_event_loop.h"
+//#include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "driver/uart.h"
